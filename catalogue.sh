18f936fc6bf479d60d9016e2481856015487e9bc
Sai Kumar Gandikota <saikumarcloud8@gmail.com>
2025-07-19 13:24:51 +0530

    adding all 3 files

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ dnf install nodejs -y &>> $LOGFILE
 VALIDATE $? "Installing NOdeJS:18"
 
 id roboshop #if roboshop user does not exist, then it is failed
-if [ $? -ne 0 ]
+if [ $? -ne 0 ] 
 then 
     useradd roboshop
     VALIDATE $? "roboshop user creation"
